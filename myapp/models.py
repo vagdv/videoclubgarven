da3c73ac8741222687dc81010fe062d6e1977096
Andoni <a.ventura@upm.es>
2021-04-02 12:49:53 +0200

    myapp config + movie model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    urlTrailer = models.CharField(max_length=100)
+    desc = models.CharField(max_length=400)
+    year = models.DateField()
+    director = models.CharField(max_length=100)
+    cast = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    rating = models.IntegerField
+
+    def __str__(self):
+        return self.name
