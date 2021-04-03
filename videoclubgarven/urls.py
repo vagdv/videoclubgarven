c527c4629f52a99e78a676f4d579a4417337d939
Andoni <a.ventura@upm.es>
2021-04-03 13:20:23 +0200

    Auto stash before merge of "main" and "origin/main"

--- videoclubgarven/urls.py
+++ videoclubgarven/urls.py
@@ -19,9 +19,15 @@ from myapp import views
 
 admin.site.site_header = "Garven Admin"
 
+LOGIN_URL = '/myproject/login/'
+
+LOGOUT_REDIRECT_URL = '/myproject/login/'
+
 urlpatterns = [
     path('', admin.site.urls),
     path('movie/<int:movie_id>/', views.movie, name='movie'),
     path('movies', views.index, name='index'),
     path('loginn', views.login, name='loginhector')
 ]
+
+
