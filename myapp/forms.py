4b53dfd7b4112df405dd467ac7fe6b9854860009
Andoni <a.ventura@upm.es>
2021-05-23 00:10:56 +0200

    movie form

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Movie
 from django import forms
 
 
@@ -8,3 +9,9 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class MovieForm(ModelForm):
+    class Meta:
+        model = Movie
+        fields = '__all__'
