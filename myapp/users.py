ff3c89292e12d2ce427f5ad9943df67c70abc5b9
Andoni <a.ventura@upm.es>
2021-05-22 20:43:05 +0200

    new user

--- myapp/users.py
+++ myapp/users.py
@@ -2,24 +2,17 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from .forms import RegisterUserForm
 from django.core.paginator import Paginator
-
-
-# @login_required(login_url='login')
-# def movie(request, movie_id):
-#     try:
-#         movie = Movie.objects.get(pk=movie_id)
-#     except Movie.DoesNotExist:
-#         raise Http404("Movie does not exist")
-#     return render(request, 'myapp/movie/movie.html', {'movie': movie})
 
 
 @login_required(login_url='login')
 def index(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
     User = get_user_model()
     users = User.objects.all()
-
-    #paginator = Paginator(users, 3)
-    #page_obj = paginator.get_page(page_number)
 
     return render(request, 'myapp/crud/users.html', {'users': users})
